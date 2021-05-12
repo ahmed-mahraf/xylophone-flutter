398f1dba8fdfb0582288b02204cd3953df3fccd9
Mahraf Ahmed <ahmed.mahraf@outlook.com>
2021-05-12 13:50:13 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MainPage(),
+    ),
+  );
+}
+
+class MainPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
