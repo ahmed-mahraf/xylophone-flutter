1ffacd52a315a5bf894defecd1ed6c656b640d48
Mahraf Ahmed <ahmed.mahraf@outlook.com>
2021-05-15 09:02:41 +0100

    Add sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MainPage(),
-    ),
+    MainPage(),
   );
 }
 
 class MainPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('nouns.last'),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
